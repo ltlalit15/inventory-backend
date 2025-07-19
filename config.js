87dbcd36ca6c3ae0cef88de2af5e9c24b9f00fd5
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-07-19 16:00:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,37 +1,18 @@
-// local mysql
+// live server
 
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-    host: '127.0.0.1',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'inventory',
-    multipleStatements: true
+  host: 'ballast.proxy.rlwy.net',
+  port: 19598,
+  user: 'root',
+  password: 'wmnatvbnKaNnCTXDJZoyfrsErUboJPpk',
+  database: 'railway', // Replace with actual DB name if different
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
- 
-console.warn('Connected');
-
+console.warn('✅ Connected to Railway MySQL');
 module.exports = db;
-
-
-// live server
-
-// const mysql = require('mysql2/promise');
-
-// const db = mysql.createPool({
-//     host: 'nozomi.proxy.rlwy.net',        // ✅ Remote host from CLI
-//     port: 48488,                           // ✅ Port from CLI
-//     user: 'root',                          // ✅ Username
-//     password: 'IgqaBOnvVntHIquaIjMjvATrnuEuRZSz',  // ✅ Password
-//     database: 'railway',                  // ✅ Database name
-//     multipleStatements: true              // Optional, allows running multiple queries
-// });
-
-// console.warn('✅ Connected to Railway MySQL');
-
-// module.exports = db;
-
 
