33db8c8f2b4b9d7876a798d7d9e5eba1541c23ac
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-07-26 14:57:00 +0530

    Update stripeRoutes.js

--- routes/stripeRoutes.js
+++ routes/stripeRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createCartPayment, getAllUserCartPaymentData, deletePaymentById } = require('../controller/stripe');
+const { createCartPayment, getAllUserCartPaymentData, deletePaymentById, confirmPaymentStatus } = require('../controller/stripe');
 const authMiddleware = require('../middleware/authMiddleware');
 const multer = require('multer');
 const path = require('path');
@@ -9,6 +9,7 @@ const router = express.Router();
 router.post('/createCartPayment', createCartPayment);
 router.get('/getAllUserCartPaymentData',  getAllUserCartPaymentData);
 router.delete('/deletePaymentById/:id',  deletePaymentById);
+router.post('/confirmPaymentStatus', confirmPaymentStatus);
 
 
 module.exports = router;
